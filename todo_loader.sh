f77665859cc0b77c314935aba9efd300c9e742cd
Ning Wang <ningwang1729@gmail.com>
2021-11-20 21:52:24 -0800

    Improved clarity for todo app launcher

--- todo_loader.sh
+++ todo_loader.sh
@@ -2,9 +2,12 @@
 
 TODO_COUNT=`ps | grep "todo_app.sh" | wc -l`
 
+cd ${TODO_LIST_PATH}
+
 if (( ${TODO_COUNT} > 1 )); then
-    echo "TODO App is active…"
+	echo "TODO App is active…"
+	cd ~/
 else
-    echo "Loading TODO App…"
-    ${TODO_LIST_PATH}/todo_app.sh
+	echo "Loading TODO App…"
+	${TODO_LIST_PATH}/todo_app.sh
 fi
